a1c812012fb23d89dc3cc8d4017891332b617c53
Joni <n0anjo00@students.oamk.fi>
2021-01-22 16:45:59 +0200

    tallennus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+  const [age, setAge] = useState(null)
+  const [hrl, setHrl] = useState(0)
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const upper = ((220-age)*0.85).toFixed(0);
+    const lower = ((220-age)*0.65).toFixed(0);
+    setHrl(lower + "-" + upper);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 class="pt-4">Heart rate limits calculator</h3>
+    <form onSubmit={handleSubmit}>
+      <div class="form-group">
+        <label class="pt-3">Age</label>
+        <input class="form-control" type="number" value={age} onChange={e => setAge(e.target.value)}></input>
+      </div>
+      <div class="form-group">
+        <label>Heart rate limits</label>
+        <output  class="form-control">{hrl}</output>
+      </div>
+      <button class="btn btn-primary">Calculate</button>
+    </form>
     </div>
   );
 }
